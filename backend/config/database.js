33a14061843a6cc512188c4cd6f17e28a4358b63
hikmalhaqiqi <hikmalstudent@gmail.com>
2025-04-13 09:34:13 +0700

    ubahdb

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('RecoverDB', 'root', '', {
+const db = new Sequelize('RECOVER_YOUR_DATA', 'root', '', {
     host: '104.197.117.136',
     dialect: 'mysql',
 
